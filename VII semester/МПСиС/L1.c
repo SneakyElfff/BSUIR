67ce0a05fe482f43023e80e49aeeaaa30b07ff81
Nina <alhimovich.nina@gmail.com>
2024-09-19 10:58:43 +0300

    Add files via upload

--- VII semester/МПСиС/L1.c
+++ VII semester/МПСиС/L1.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main.c
+ * L1.c
  */
 int main(void)
 {
@@ -10,37 +10,52 @@ int main(void)
 
     WDTCTL = WDTPW | WDTHOLD;    // остановить сторжевой таймер
 
+    // настройка порта для диода LED3 (P8.2)
+    P8DIR |= BIT2;    // P8.2 (LED3) как выход
+    P8OUT &= ~BIT2;    // выключить LED3
+
     // настройка порта для кнопки S1 (P1.7)
     P1DIR &= ~BIT7;    // P1.7 (S1) как вход
-    P1REN |= BIT7;    // подтягивающий резистор
-    P1OUT |= BIT7;    // подтягивание к Vcc
-
-    // настройка порта для диода LED3 (P8.2)
-    P8DIR |= BIT2;    // P8.2 (S1) как выход
-    P8OUT &= ~BIT2;    // выключить LED3
+    P1REN |= BIT7;    // включить подтягивающий резистор
+    P1OUT |= BIT7;    // подтягивание на P1.7
 
     while(1)
     {
-        if(!(P1IN & BIT7))    // если кнопка нажата (P1.7 = 0)
+        if(P1IN & BIT7)    // если кнопка нажата
         {
-            if(button_state == 0)
-            {
-                P8OUT |= BIT2;    // включить LED3
-                __delay_cycles(50000);    // задержка импульса
-                P8OUT &= ~BIT2;    // выключить LED3
+            __delay_cycles(100000);    // задержка импульса для предотвращения дребезга кнопки
 
-                button_state = 1;
+            if(P1IN & BIT7)
+            {
+                if(button_state == 0)
+                {
+                    button_state = 1;
+
+                    P8OUT |= BIT2;    // включить LED3
+
+                    __delay_cycles(100000);
+
+                    P8OUT &= ~BIT2;    // выключить LED3
+                }
             }
         }
-        else    // если кнопка отпущена (P1.7 = 1)
-        {
-            if(button_state == 1)
-            {
-                P8OUT |= BIT2;    // включить LED3
-                __delay_cycles(50000);    // задержка импульса
-                P8OUT &= ~BIT2;    // выключить LED3
 
-                button_state = 0;
+        if(!(P1IN & BIT7))    // если кнопка отпущена
+        {
+            __delay_cycles(100000);
+
+            if(!(P1IN & BIT7))
+            {
+                if(button_state == 1)
+                {
+                    P8OUT |= BIT2;    // включить LED3
+
+                    __delay_cycles(100000);    // задержка импульса
+
+                    P8OUT &= ~BIT2;    // выключить LED3
+
+                    button_state = 0;
+                }
             }
         }
     }
